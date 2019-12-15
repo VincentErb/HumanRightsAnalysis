a7f6b70fd36eeca1c6c33d23b4d66bca35bf202a
Vincent Erb <vincent.erb@hotmail.fr>
2019-12-15 13:10:39 +0100

    follow up init, ready to work

---
+++ main.r
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(plyr)
+library(scales)
+library(reshape2)
+
+MyData <- read.csv2(file="C:/Users/vince/Documents/INSA/5ISS/BigData/HumanRightsAnalysis/dataset.csv", header=TRUE, sep=",", fileEncoding = 'UTF-8')
+
+
