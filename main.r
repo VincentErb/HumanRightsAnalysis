3294e1271b48af5f906e007d2d9263f025a3af27
Vincent Erb <vincent.erb@hotmail.fr>
2019-12-18 14:41:02 +0100

    formatting for r code

--- main.r
+++ main.r
@@ -1,8 +1,30 @@
+# General imports
 library(ggplot2)
 library(plyr)
 library(scales)
 library(reshape2)
 
+# CSV Dataset import
 HRFData <- read.csv2(file="C:/Users/vince/Documents/INSA/5ISS/BigData/HumanRightsAnalysis/dataset.csv", header=TRUE, sep=",", fileEncoding = 'UTF-8')
 
+# =============== Data manipulation =============== # 
+# 1st chart
 
+# 2nd chart
+
+# 3rd chart
+
+# 4th chart
+
+# 5th chart
+
+# ==================== Plotting =================== # 
+# 1st chart
+
+# 2nd chart
+
+# 3rd chart
+
+# 4th chart
+
+# 5th chart
